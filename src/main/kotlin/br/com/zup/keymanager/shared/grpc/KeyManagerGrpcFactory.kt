de0e6bcf950a2ca063fb2aeb9ef849a4b3898b06
Jonathan Pereira Dias <52224050+JhonDias98@users.noreply.github.com>
2021-06-19 01:40:03 -0300

    Expondo para o frontend: Registro da chave Pix

---
+++ src/main/kotlin/br/com/zup/keymanager/shared/grpc/KeyManagerGrpcFactory.kt
@@ -0,0 +1,13 @@
+package br.com.zup.keymanager.shared.grpc
+
+import br.com.zup.KeymanagerRegistraGrpcServiceGrpc
+import io.grpc.ManagedChannel
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import javax.inject.Singleton
+
+@Factory
+class KeyManagerGrpcFactory(@GrpcChannel("KeyManager") val channel: ManagedChannel) {
+    @Singleton
+    fun registraChave() = KeymanagerRegistraGrpcServiceGrpc.newBlockingStub(channel)
+}
